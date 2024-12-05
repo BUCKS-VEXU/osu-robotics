8d281a43355c95add0f569ebbdbf2605b2d935e2
NoahK216 <115042520+NoahK216@users.noreply.github.com>
2024-12-05 12:21:56 -0500

    Added new sponsors

--- src/routes/Home/Sections/Sponsors.tsx
+++ src/routes/Home/Sections/Sponsors.tsx
@@ -6,7 +6,10 @@ import './Sponsors.css';
 const Sponsors = () => {
     const sponsors = [
         { name: 'Eaton', logo: 'media/sponsors/eaton-logo-transparent.png', href: 'https://www.eaton.com/us/en-us.html' },
+        { name: 'Polymaker', logo: 'media/sponsors/Polymaker-logo.png', href: 'https://polymaker.com/' },
         { name: 'Joseph and Joseph', logo: 'media/sponsors/joseph-joseph-logo-transparent.png', href: 'https://josephcpas.com/' },
+        { name: 'GK Properties', logo: 'media/sponsors/GK-Properties-logo.png', href: '' },
+        { name: 'KHM Travel Group', logo: 'media/sponsors/KHM-logo.png', href: '' },
     ];
 
     return (
