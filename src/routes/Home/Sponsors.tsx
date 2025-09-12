357b4b6652a9bc4df057c6a4be84319bae37ae4b
NoahK216 <115042520+NoahK216@users.noreply.github.com>
2025-09-12 12:41:05 -0400

    Remove old sponsors

--- src/routes/Home/Sponsors.tsx
+++ src/routes/Home/Sponsors.tsx
@@ -5,11 +5,8 @@ import './Sponsors.css';
 
 const Sponsors = () => {
     const sponsors = [
-        { name: 'Eaton', logo: 'assets/sponsors/eaton-logo-transparent.png', href: 'https://www.eaton.com/us/en-us.html' },
         { name: 'Polymaker', logo: 'assets/sponsors/Polymaker-logo.png', href: 'https://polymaker.com/' },
         { name: 'Joseph and Joseph', logo: 'assets/sponsors/joseph-joseph-logo-transparent.png', href: 'https://josephcpas.com/' },
-        { name: 'GK Properties', logo: 'assets/sponsors/GK-Properties-logo.png', href: 'https://www.gkpropertieslv.com/' },
-        { name: 'KHM Travel Group', logo: 'assets/sponsors/KHM-logo.png', href: 'https://khmtravel.com/' },
         { name: 'Digital Sports Solutions', logo: 'assets/sponsors/DSS_CombinationMark_FullColor.svg', href: 'https://www.digitalsportssolutions.com/' },
     ];
 
